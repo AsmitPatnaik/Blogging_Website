d9b2c8b3583f39256cb9f24c7c2506be7d1c161a
AsmitPatnaik <142527625+AsmitPatnaik@users.noreply.github.com>
2025-02-03 19:55:48 +0530

    dark mode js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+//post filtering
 function filterPosts() {
     const selectedYear = document.querySelector('input[name="year"]:checked')?.value;
     const posts = document.querySelectorAll('.home-article');
@@ -11,3 +13,25 @@ function filterPosts() {
         }
     });
 }
+
+
+
+//dark mode
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
+
+// Check local storage for user preference
+if (localStorage.getItem("darkMode") === "enabled") {
+    body.classList.add("dark-mode");
+}
+
+darkModeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    // Save user preference in local storage
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
+});
